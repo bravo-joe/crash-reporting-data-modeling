4766a656ccbedc59fe911c3e448f2da5d3804c13
Joe Bravo <joepbravo@gmail.com>
2023-12-15 23:40:51 -0800

    Build workflow for rf model

--- multiple_workflows.r
+++ multiple_workflows.r
@@ -2,11 +2,12 @@
 # Import necessary libraries
 library(dplyr) # for functions
 library(data.table)
-install.packages("tidymodels")
+#install.packages("tidymodels")
 library(tidymodels)
 tidymodels_prefer()
+require(caTools)
+library(parsnip)
 set.seed(120)  # Setting seed
-
 
 # Import the dataset
 raw_data <- fread("/Volumes/my_passport/data_sets/WA_Crash_Summary.csv")
@@ -28,11 +29,27 @@ sample <- sample.split(processed_df$"Injury Severity", SplitRatio = 0.80)
 train_df <- subset(processed_df, sample == TRUE)
 test_df  <- subset(processed_df, sample == FALSE)
 
+# Initiate the random forest model
+rf_model_1 <-
+    rand_forest(trees = 500) %>%
+    set_engine("ranger") %>%
+    set_mode("classification")
+#rf_model_1
 
+# Setup a workflow
+rf_wflow_1 <-
+    workflow() %>%
+    add_model(rf_model_1)
 
-
-
-
+# Now, add a formula as a preprocessor
+rf_wflow_1 <-
+    rf_wflow_1 %>%
+    add_formula(
+        "Injury Severity" ~
+        "Weather Condition" +
+        "Lighting Condition" +
+        "City"
+    )
 
 
 
