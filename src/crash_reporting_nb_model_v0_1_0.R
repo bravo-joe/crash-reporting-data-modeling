314dee37b114ecbbff2c26916ef419cd8439f280
Jose Bravo <joepbravo@gmail.com>
2024-02-01 20:47:36 -0800

    Trained model on test data and got predictions

--- src/crash_reporting_nb_model_v0_1_0.R
+++ src/crash_reporting_nb_model_v0_1_0.R
@@ -70,12 +70,12 @@ splits <- initial_split(subset_df, prop = 2/3, strata = injury_severity)
 train_split <- training(splits)
 test_split <- testing(splits)
 
+###################################################
 # 2. Prep recipe
-nb_rec <-
+nb_recipe <-
   recipe(injury_severity  ~ ., data = train_split) %>%
-  step_upsample(injury_severity, over_ratio = 1.0) %>%
-  step_dummy(weather, light, vehicle_body_type)
-nb_rec
+  step_upsample(injury_severity, over_ratio = 0.75)
+nb_recipe
 
 # 3. Specify model
 # Naive Bayes Model Specifications
@@ -90,124 +90,25 @@ nb_model
 # 4. Set workflow (Preprocess & model)
 nb_wflow <-
   workflow() %>%
-  add_model(naive_bayes_model) %>%
-  add_recipe(nb_rec)
+  add_model(nb_model) %>%
+  add_recipe(nb_recipe)
 nb_wflow
 
-# 5. An alternative approach using prep()
-crash_rec_trained <- prep(nb_rec)
-crash_rec_trained
+# 5. Fit the training data
+nb_fit <- fit(nb_wflow, train_split)
+nb_fit
 
-show_variables <-
-  nb_rec %>%
-  prep(log_changes = TRUE)
+nb_fit %>% extract_fit_engine()
 
-# 2b. Prep recipe
-nb_rec <-
-  train_split %>%
-  recipe(injury_severity  ~ .) %>%
-  step_upsample(injury_severity, over_ratio = 1.0) %>%
-  step_dummy(weather, light, vehicle_body_type)
-nb_rec
+# 6. Predict from workflow object on new data
+nb_pred <- predict(nb_fit, test_split, type = "class")
+nb_pred
 
-# 3b. Apply transformation to the data
-crash_data_transformed <-
-  prep(nb_rec) %>%
-  bake(new_data = train_split)
-crash_data_transformed
-
-# 4b. Apply bake() function
-crash_val_processed <- bake(crash_rec_trained, new_data = test_split)
-crash_val_processed
-
-#crash_validation <- test_split$splits %>% pluck(1) %>% assessment()
-#crash_validation
-
-p2 <-
-  crash_val_processed %>%
-  ggplot(aes(x = injury_severity)) +
-  geom_bar() +
-  ggtitle("Processed validation set data")
-p2
-  
-# Apply model recipe to new data
-test_data_transformed <-
-  prep(nb_rec) %>%
-  bake(new_data = test_split)
-
-# Store predictions/results in its own variable
-result <-
-  predict(
-    object = extract_fit_parsnip(
-      nb_model
-    ),
-    new_data = test_data_transformed,
-    type = "class"
-  )
-
-###################################################
-# Will attempt different approach to modeling
-# Create 2 custom functions
-###################################################
-# Create prediction tables
-predict_table <- function(model, data, tidy_flag){
-  if (tidy_flag == TRUE) {
-    result <- model %>%
-      predict(data) %>%
-      rename(pred = .pred) %>%
-      mutate(
-        actual = data$total,
-        pred_real = pred^3,
-        actual_real = actual^3
-      )
-  } else {
-    result <- model %>%
-      predict(data) %>%
-      as_tibble_col(column_name = "pred") %>%
-      mutate(
-        actual = data$total,
-        pred_real = pred^3,
-        actual_real = actual^3
-      )
-  }
-  result
-}
-
-nb_model <- naive_Bayes(
-  mode = "classification",
-  smoothness = tune(),
-  Laplace = tune(),
-  engine = "naivebayes"
-)
-
-nb_wflow <-
-  workflow() %>%
-  add_model(naive_bayes_model) %>%
-  add_recipe(nb_rec)
-
-# Folds
-severity_folds <- vfold_cv(train_split, v = 10, strata = injury_severity)
-# Performance metrics - classification model
-perf_metrics <- metric_set(roc_auc, recall, precision, f_meas)
-# Control Object
-ctrl_obj <- control_grid(verbose = FALSE, save_pred = TRUE)
-
-nb_tune <- nb_wflow %>%
-  tune_grid(
-    resamples = severity_folds,
-    metrics = perf_metrics,
-    control = ctrl_obj
-  )
-
-# Fit with best parameters
-nb_best <-
-  finalize_workflow(nb_wflow, select_best(nb_tune)) %>%
-  fit(train_split)
-
-
-
+# 7. Create a new table with actual and predicted values
+results_tbl <- bind_cols(actual = test_split$injury_severity, predicted = nb_pred$.pred_class)
+results_tbl
 
 # Cleanup
-unlink("data_folder", recursive = TRUE)
+#unlink("data_folder", recursive = TRUE)
 
 # End of crash_reporting_nb_model_v0_1_0.R
